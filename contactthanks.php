93d7f4096c1eea34d054a3040fd9b8c900b1ef62
martinmckenna <mmckenna.phila@gmail.com>
2014-05-16 13:10:33 -0400

    testing new pages

---
+++ contactthanks.php
@@ -0,0 +1,47 @@
+   <body>
+    <link rel="stylesheet" type="text/css" href="contact.css" />
+     <div class = "header">
+      <div id = "headercontent">
+      <a id = "logo" href="http://martinmckenna.github.io/"><img src="http://i.imgur.com/3SPYzZo.png" title="Hosted by imgur.com"/></a>
+  </div>
+    </div>
+      <div class = "navbar">
+        <ul>
+          <li id = "home">
+            <a href="http://martinmckenna.github.io/">Home</a>
+          </li>
+          <li id = "resume">
+            <a href="http://martinmckenna.github.io/resume">Resume</a>
+          </li>
+          <li id = "projects">
+            <a href="http://martinmckenna.github.io/">Projects</a>
+          </li>
+          <li id = "Contact">
+            <a href="http://martinmckenna.github.io/">Contact</a>
+          </li>
+            </ul>
+            </div>
+     <div class = "container">
+       <div id = "thanks">
+       <h1>Your message has been sent!</h1><br />
+		
+		<p><a href="contact.html">Back to Contact Form</a></p>
+	
+	</div>
+	
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script>
+	<script type="text/javascript">
+	_uacct = "UA-68528-29";
+	urchinTracker();
+	</script>
+
+</body>
+
+</html>
+     </div>
+     </div>
+  <div class = "footer">
+    <a href = "https://twitter.com/Marty_McKenna" target="_blank"><img src = "http://i.imgur.com/2wgMtAD.png"/></a>
+    </footer>
+</body>
